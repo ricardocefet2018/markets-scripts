41de1d1a8105ffcaf7a133d8ffdb61ccb759bc6a
Ricardo Rocha <ricardocefet2018@gmail.com>
2023-08-06 10:39:41 -0300

    fix error listing items waxpeer

--- listItemsWaxpeer.js
+++ listItemsWaxpeer.js
@@ -4,5 +4,5 @@ for (const div of divs) {
   let inputPreco = div.querySelector('input');
   let precoSteam = Number(div.querySelector('div.f.item_bottom > div.f.prices > div > p > span').innerText);
   inputPreco.value = (precoSteam*5).toString();
-  inputPreco.dispatchEvent(new Event('input'));
+  inputPreco.dispatchEvent(new Event('change'));
 }
