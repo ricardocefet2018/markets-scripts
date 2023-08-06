e9c8a0b79331bf41bd45bdb9c8472b7e173e4b87
Ricardo Rocha <ricardocefet2018@gmail.com>
2023-08-06 17:53:19 -0300

    fix error getting second page

--- getListFromDmarket.js
+++ getListFromDmarket.js
@@ -1,11 +1,7 @@
 let rate = 0.1393184; // 1 CNY em USD
-let multiplier = 1 * 1.05 * 1.017; // Multiplicação das suas taxas para receber o valor real (1.05 é a taxa de saque do shadowpay) 
+let multiplier = 1 * 1.05 * 1.017; // Multiplicação das suas taxas para receber o valor real (1.05 é a taxa de saque do shadowpay)
 
-let items = [
-  ...document.querySelectorAll(
-    "#mat-dialog-0 > flow-dialog > flow-stepper > history-dialog > div > div.c-dialog__scroll > app-history > div > div.c-history__table.ng-star-inserted > app-history-item"
-  ),
-];
+let items = [...document.querySelectorAll("app-history-item")];
 
 let purchases = items.map((item) => {
   let unformattedDH = item.querySelectorAll("div")[0].innerText;
